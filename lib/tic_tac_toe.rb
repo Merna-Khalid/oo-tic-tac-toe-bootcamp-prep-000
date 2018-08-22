a762835708ef1ec38d54c6109af11c92ce393ee7
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-22 00:49:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,15 +10,19 @@ class TicTacToe
     [0, 4, 8],
     [2, 4, 6]
   ]
+  
   def initialize(board=nil)
     @board = board || Array.new(9, " ")
   end
+  
   def turn_count
     @board.count { |cell| cell == 'X' || cell == 'O'}
   end
+  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -26,18 +30,23 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
   def input_to_index(user_input)
     user_input.to_i - 1
   end
+  
   def move(index, current_player)
     @board[index] = current_player
   end
+  
   def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end
+  
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(@board, index)
   end
+  
   def check(board, token='X')
     return state = WIN_COMBINATIONS.detect do |one_state|
       one_state.all? { |i| board[i] == token}
